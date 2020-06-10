fbb23f565a30f3b4b2b1c29426dca7cab981ba67
brenobccc <brenobritobc@gmail.com>
2020-06-10 08:45:05 -0300

    Mudança no topo da HomeScreenTab

--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -3,16 +3,33 @@ import 'package:flutter/material.dart';
 class HomeTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     Widget _buildBodyBack() => Container(
-      color: Colors.blueAccent[100],
-    
-    );
-    
-    return Stack(
+          color: Colors.blueAccent[100],
+        );
 
+    return Stack(
       children: <Widget>[
-        _buildBodyBack(),
+        ClipPath(
+          child: Container(
+              
+              height: 170,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(40.0),
+                  
+                  bottomRight: Radius.circular(40.0),
+                ),
+                  gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                
+                colors: [
+                  Color(0xFF3383CD),
+                  Color(0xFF11249F),
+                ],
+              ))),
+        ),
         CustomScrollView(
           slivers: <Widget>[
             SliverAppBar(
@@ -21,16 +38,19 @@ class HomeTab extends StatelessWidget {
               backgroundColor: Colors.transparent,
               elevation: 0.0,
               flexibleSpace: FlexibleSpaceBar(
-                title: const Text("Home",style: TextStyle(color: Colors.white,fontSize:28,),),
+                title: const Text(
+                  "Home",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 28,
+                  ),
+                ),
                 centerTitle: true,
               ),
-
             ),
           ],
         ),
-        
       ],
-      
     );
   }
 }
