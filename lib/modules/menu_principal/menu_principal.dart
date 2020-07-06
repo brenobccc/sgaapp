9a5bf06ed62a1c4686b9dcd1a489794cc76c73bc
renanmoura100 <naneraruom@gmail.com>
2020-07-06 11:49:19 -0300

    gaveta

--- lib/modules/menu_principal/menu_principal.dart
+++ lib/modules/menu_principal/menu_principal.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:sgaapp/models/user_model.dart';
+import 'package:sgaapp/modules/login/page/login_screen.dart';
 import 'package:sgaapp/modules/menu_principal/utils/telas.dart';
 
 class MenuPrincpal extends StatefulWidget {
@@ -8,9 +10,37 @@ class MenuPrincpal extends StatefulWidget {
 
 class _MenuPrincpalState extends State<MenuPrincpal> {
   int indice = 0;
+  UserModel model;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Drawer Header'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            GestureDetector(
+              child: Text(
+                "Sair",
+                style: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              onTap: () {
+                model.signOut();
+                Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(builder: (context) => LoginScreen()));
+              },
+            ),
+          ],
+        ),
+      ),
       body: TELAS[indice],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: indice,
