ee5c4fc301b9c283a566c251bc0472205cb5e961
renanmoura100 <naneraruom@gmail.com>
2020-07-08 11:49:12 -0300

    color sub cards

--- lib/components/sub_cards.dart
+++ lib/components/sub_cards.dart
@@ -7,9 +7,16 @@ class SubCard extends StatelessWidget {
   final String rota;
   final Map<dynamic, dynamic> args;
   final String imagem;
+  final Color color;
 
   SubCard(
-      {Key key, this.titulo, this.onPressed, this.rota, this.args, this.imagem})
+      {Key key,
+      this.titulo,
+      this.onPressed,
+      this.rota,
+      this.args,
+      this.imagem,
+      this.color})
       : super(key: key);
 
   @override
@@ -17,34 +24,44 @@ class SubCard extends StatelessWidget {
     double height = MediaQuery.of(context).size.height;
     return InkWell(
       onTap: () {
-        //print(args["dados"].imagem);
         Navigator.pushNamed(context, rota, arguments: args);
       },
       child: Material(
-        color: Colors.white,
+        //color: color ?? Color.fromRGBO(245, 207, 79, 0.5),
         borderRadius: BorderRadius.circular(15),
         elevation: 3,
         child: Column(
           children: <Widget>[
             Container(
                 padding: EdgeInsets.symmetric(horizontal: 10),
-                height: height * 0.2,
+                height: height * 0.18,
                 width: double.infinity,
                 decoration: BoxDecoration(
                   borderRadius:
                       BorderRadius.only(topLeft: border, topRight: border),
-                  color: Colors.green,
+                  color: color ?? Color.fromRGBO(245, 207, 79, 0.9),
                 ),
-                child: Center(child: Image.asset(imagem ?? "assets/bee.png")
-
-                    //      Text(
-                    //   titulo ?? "sem descrição",
-                    //   style: TextStyle(fontSize: 20),
-                    // )
-
-                    )),
-            Container(
-              child: Text(titulo ?? "sem descrição"),
+                child: Center(
+                    child: Padding(
+                  padding: const EdgeInsets.all(3.0),
+                  child: Image.asset(imagem ?? "assets/bee.png"),
+                ))),
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius:
+                      BorderRadius.only(bottomLeft: border, bottomRight: border),
+                  color: color ?? Color.fromRGBO(245, 207, 79, 0.6),
+                ),
+                padding: EdgeInsets.all(5),
+                child: Text(
+                  titulo ?? "sem descrição",
+                  overflow: TextOverflow.ellipsis,
+                  textAlign: TextAlign.left,
+                  style: TextStyle(color: Colors.white, fontSize: 15),
+                ),
+              ),
             ),
           ],
         ),
