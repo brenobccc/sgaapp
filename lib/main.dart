a73308b2b41cc13b2d2108d9b96682d4ff363fd5
renanmoura100 <naneraruom@gmail.com>
2020-07-09 09:42:35 -0300

    bloquear rotação

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get_it/get_it.dart';
 import 'package:scoped_model/scoped_model.dart';
 import 'package:sgaapp/models/user_model.dart';
@@ -23,6 +24,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     return ScopedModel<UserModel>(
       model: UserModel(),
       child: MaterialApp(
