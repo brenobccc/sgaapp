811f353cc94c50216657543473af9ad8103c5744
brenobccc <brenobritobc@gmail.com>
2020-07-10 10:36:16 -0300

    altereiOnePressedButãoParaNulo(AppBarTractor)

--- lib/modules/menu_principal/menu_principal.dart
+++ lib/modules/menu_principal/menu_principal.dart
@@ -29,7 +29,7 @@ class _MenuPrincpalState extends State<MenuPrincpal> {
               "assets/icones/icone_logo.svg",
               height: 25,
             ),
-            onPressed: () {},
+            onPressed: null,
           ),
         ),
         actions: <Widget>[
